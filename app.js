2b99baafacafdab2df70f26f724b8f3ca4fd0da3
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-10 15:49:58 +0100

    Server error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const FilmsRouter = require('./routes/film')
 const NotFound = require('./middleware/notFound')
+const ServerErrorsHandler = require('./middlewares/ServerErrorsHandler')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
@@ -17,4 +18,7 @@ server.get('/', (req, res) => {
 
 server.use('/movies', FilmsRouter)
 
+// Gestire gli errori 404 
 server.use(NotFound)
+// gestire tutti gli errori 500
+server.use(ServerErrorsHandler)
