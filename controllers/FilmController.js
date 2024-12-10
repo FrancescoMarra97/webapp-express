5c6c6fecbe9390610c42e0ecbd6eda10b308df72
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-10 15:21:12 +0100

    function show

--- controllers/FilmController.js
+++ controllers/FilmController.js
@@ -1,4 +1,4 @@
-const connection = require('../database/connection');
+const connection = require('../database/connection.js');
 
 // index
 
@@ -13,6 +13,40 @@ function index(req, res) {
     })
 }
 
+//show
+function show(req, res) {
+    const id = req.params.id
+    const sql = `SELECT * FROM movies WHERE id=?`
+
+    //tutte le recensioni per un film
+
+    const reviewsSql = `SELECT * FROM reviews WHERE movie_id=?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        if (results.length == 0) return res.status(404).json({ err: 'movie not found' })
+    })
+
+    //tutte le recensioni associate ai film
+
+    connection.query(reviewsSql, [id], (err, reviewsResults) => {
+        if (err) return res.status(500).json({ err: err })
+
+
+        const film = {
+            ...results[0],
+            reviews: reviewsResults
+        }
+
+        res.json(film)
+
+    })
+
+
+
+}
+
 module.exports = {
-    index
+    index,
+    show
 }
--- routes/film.js
+++ routes/film.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const FilmController = require('../controllers/FilmController')
 
+//tutti i film
 router.get('/', FilmController.index);
+
+// un singolo film
+router.get('/:id', FilmController.show)
+
 
 module.exports = router;
