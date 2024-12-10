6591cfd2cebddccc4bb83c2a106487130268a434
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-10 12:57:20 +0100

    index

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
 
+const FilmsRouter = require('./routes/film')
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
@@ -8,3 +10,9 @@ const PORT = process.env.PORT
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${HOST}:${PORT}`);
 })
+// Routes 
+server.get('/', (req, res) => {
+    res.send(`Server is up and running!`);
+})
+
+server.use('/movies', FilmsRouter)
