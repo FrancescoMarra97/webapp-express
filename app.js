f54409e072cd3672db35e13a0deb7d73735b1b41
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-12 12:30:55 +0100

    cors e body parse

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const server = express();
 const FilmsRouter = require('./routes/film')
 const NotFound = require('./middleware/notFound')
-const ServerErrorsHandler = require('./middlewares/ServerErrorsHandler')
+const ServerErrorsHandler = require('./middleware/ServerErrorsHandler')
+const cors = require('cors')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-
+server.use(cors())
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${HOST}:${PORT}`);
 })
@@ -15,7 +16,7 @@ server.listen(PORT, () => {
 server.get('/', (req, res) => {
     res.send(`Server is up and running!`);
 })
-
+server.use(express.json());
 server.use('/movies', FilmsRouter)
 
 // Gestire gli errori 404 
